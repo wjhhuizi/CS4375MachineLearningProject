c1179eb7b57778f95e8e2d687c66bd61f122bb5d
Michael <wjh.huizi@gmail.com>
2018-11-24 12:09:09 -0600

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
@@ -20,8 +21,11 @@ public class Main {
 
     public static void generate_ARFF(File attributeFile, File dataFile) throws IOException{
 
-        String header = "";
-        String data   = "";
+        String header   = "";
+        String data     = "";
+        String attrName = "";
+        String attrType = "";
+        boolean containsMissingValue = false;
 
         // Prepare ARFF File header
         sc = new Scanner(attributeFile);
@@ -30,9 +34,36 @@ public class Main {
 
         while (sc.hasNextLine()) {
             String   attr_line = sc.nextLine();
+
+            // Check if is the last line, true -> class line.
+            if (!sc.hasNextLine()){
+                break;
+            }
+
             String[] temp = attr_line.split(": ");
             //System.out.println(temp[0] + "--" + temp[1]);
-            Pattern  pattern1 = Pattern.compile("^cont ");
+
+            attrName = temp[0];
+
+            if (temp[1].contains("?")){
+                containsMissingValue = true;
+            }
+
+
+            if (temp[1].contains("cont")){
+                attrType = "numeric";
+            }
+            else {
+                temp[1] = temp[1].replace("(", "");
+                temp[1] = temp[1].replace(")", "");
+                temp[1] = temp[1].replace("?", "");
+                temp[1] = temp[1].replace(".", "");
+                System.out.println(temp[1]);
+
+                String num[] = temp[1].split(",");
+                attrType = "{".concat(temp[1].substring(0,temp[1].length()-1)).concat("}");
+                System.out.println(attrType);
+            }
 
         }
 
